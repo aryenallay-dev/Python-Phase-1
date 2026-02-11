d341ae350df5856f3bda74503009c4640a21270c
aryenallay-dev <aryenallay@gmail.com>
2026-02-11 18:56:08 +0530

    Update v1_multi_loop_cleaner.py

--- v1_multi_loop_cleaner.py
+++ v1_multi_loop_cleaner.py
@@ -7,32 +7,32 @@ raw_data = [
 ]
 
 def clean_data(raw_list):
-    number_score = []
+    number_score = []    #Stored only the scores of all the score keys
     for numbeers in raw_list:
         number_score.append(numbeers['score'])
 
-    str_score = list(map(str, number_score))
+    str_score = list(map(str, number_score))    #converted scores into str and removed all the char values
     for letters_remove in str_score:
         if letters_remove.isalpha():
             str_score.remove(letters_remove)
     
-    int_score = list(map(int, str_score))
+    int_score = list(map(int, str_score))   #Converted back into integers and then removed all the negative values
     for neg_remove in int_score:
         if neg_remove < 0:
             int_score.remove(neg_remove)
     
 
     
-    str_score_again = list(map(str, int_score))
-    for scores in raw_list:
+    str_score_again = list(map(str, int_score))   #Converted the values back to str to clean the original data
+    for scores in raw_list:                       #Converts all 'score' values of the original data to string to clean
         scores['score'] = str(scores['score'])
 
-    final_list = []
-    for integers in raw_list:
+    final_list = []                #Checks if the original data 'score' consists of any of the values fromthe score_list
+    for integers in raw_list:      #If it exists then those dict are appended to a final_list
         if integers['score'] in str_score_again:
             final_list.append(integers)
     
-    for last in final_list:
+    for last in final_list:   #converts the 'score' values back to int in the new clean final list
         last['score'] = int(last['score'])
     
     return final_list
@@ -59,8 +59,14 @@ def analyze_results(scores_list):
 
     return f'Total:{total}, Average: {average}, Lowest_value: {lowest}, Highest_value: {highest}'
 
-print(clean_data(raw_data))
-print(analyze_results(raw_data))
+cleaned_Score = clean_data(raw_data)     #Consist of the raw_data after being cleaned
+analyzed_result = analyze_results(raw_data)    #Consists of total, avf, highest and lowest values
+
+print(cleaned_Score)
+print(analyzed_result)
+
+
+
 
 
 
